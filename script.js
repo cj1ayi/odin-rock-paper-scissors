1dfbf7c9313741c224da69959352808abaa9c0b9
Nate Tan <robertanetanyatan@gmail.com>
2025-08-20 00:47:28 +0800

    Done initial refactoring

--- script.js
+++ script.js
@@ -28,33 +28,65 @@ function getHumanChoice()
 function playRound(humanChoice, computerChoice) 
 {
     humanChoice = humanChoice.toLowerCase();
+    const results = document.querySelector(".results");
 
     if (humanChoice === computerChoice) 
     {
-        console.log(`It's a tie! You both chose ${humanChoice}.`);
+        //console.log(`It's a tie! You both chose ${humanChoice}.`);
+        const tieResult = document.createElement("h3");
+        tieResult.textContent = `It's a tie! You both chose ${humanChoice}.`;
+        results.appendChild(tieResult);
+
     }
     
     else if ((humanChoice === "rock" && computerChoice === "scissors") || 
         (humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "scissors" && computerChoice === "paper"))
         {
-            console.log(`You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}.`);
+            //console.log(`You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}.`);
+            const winResult = document.createElement("h3");
+            winResult.textContent = `You win! ${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}.`;
+            results.appendChild(winResult);
+            
             humanScore++;
         }
     else 
         {
-            console.log(`You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice}.`);
+            //console.log(`You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice}.`);
+            const lostResult = document.createElement("h3");
+            lostResult.textContent = `You lose! ${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice}.`;
+            results.appendChild(lostResult);
             computerScore++;
         }    
+        runningScore.textContent = `Scores: Human - ${humanScore} \t Computer - ${computerScore}`;
 
 }
 
 // Play 5 Rounds
-for (let i = 0; i < 5; i++)
-{
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-}
 
-console.log(`Final Score -> Human: ${humanScore}, Computer: ${computerScore}`);
+const score = document.querySelector(".score");
+const runningScore = document.createElement("h2");
+score.appendChild(runningScore);
+
+//console.log(`Final Score -> Human: ${humanScore}, Computer: ${computerScore}`);
+
+
+// UI Logic
+// Rock
+const rockBtn = document.querySelector("#rock-btn");
+rockBtn.addEventListener("click", () => {
+    playRound("rock", getComputerChoice());
+});
+
+// Paper
+const paperBtn = document.querySelector("#paper-btn");
+paperBtn.addEventListener("click", () => {
+    playRound("paper", getComputerChoice());
+})
+
+// Scissors 
+const scissorsBtn = document.querySelector("#scissors-btn");
+scissorsBtn.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice());
+})
+
